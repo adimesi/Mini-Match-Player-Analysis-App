070b9888368dd3c380e64d82bf9d56059b5f5076
Adi Mesika <104310137+adimesi@users.noreply.github.com>
2025-11-22 11:28:01 +0200

    Clean up index.js by removing unused code
    Removed commented-out MongoDB connection code.

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const cors=require('cors');
 const mongoose=require('mongoose');
 
 const MatchRoute=require('./routes/MatchRoute');
-
-
 const app=express();
 app.use(cors());
 app.use(express.json());
@@ -13,15 +11,6 @@ dotenv.config();
 app.use('/matches',MatchRoute);
 
 const PORT=process.env.PORT||5000;
-// const MONGO_URI=process.env.MONGO_URI;
-
-// mongoose.connect(MONGO_URI,{  }).then(()=>{
-//     console.log('Connected to MongoDB');
-// }).catch((err)=>{
-//     console.error('Error connecting to MongoDB:',err);
-// });
-
-
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
